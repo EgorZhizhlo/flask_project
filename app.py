8fbbecd8460c6f8d2ece2e96beb56684a6af3543
Egor <ezhizhlo@mail.ru>
2023-12-11 23:55:14 +0300

    BETA 1

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from static.forms import RegistrationForm, LoginForm, ChangeUsername, ChangeEmail, ChangePassword, CreatePost
 from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user
 
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import MinMaxScaler
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:NeforMAL_1488@db/flask_db'
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
@@ -31,7 +38,18 @@ class POSTS(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False, nullable=False)
     author = db.Column(db.String(120), unique=False, nullable=False)
+    email_author = db.Column(db.String(120), unique=False, nullable=False)
     text = db.Column(db.String(1000), unique=False, nullable=False)
+
+
+class LSTM_m(db.Model, UserMixin):
+    __tablename__ = 'LSTM'
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, unique=False, nullable=False)
+    month = db.Column(db.String(30), unique=False, nullable=False)
+    passengers = db.Column(db.Float, unique=False, nullable=False)
+    predictions = db.Column(db.Float, unique=False, nullable=False)
+    wrong_answer = db.Column(db.Float, unique=False, nullable=False)
 
 
 with app.app_context():
@@ -60,18 +78,6 @@ def about_post(post_id):
     form1 = LoginForm()
     post = POSTS.query.filter_by(id=post_id).first()
     return render_template("about_post.html", form=form, form1=form1, admin_key=admin_key, post=post)
-
-
-@app.route('/search')
-def search():
-    search = '%{}%'.format(request.args.get('search'))
-    form = RegistrationForm()
-    form1 = LoginForm()
-    page = request.args.get('page', 1, type=int)
-    print(search)
-    posts = POSTS.query.filter(POSTS.title.like(search) | POSTS.author.like(search) | POSTS.text.like(search)).paginate(
-        page=page, per_page=3)
-    return render_template("search.html", form=form, form1=form1, admin_key=admin_key, posts=posts, search=search)
 
 
 @app.route('/about/')
@@ -268,12 +274,10 @@ def admin_change_password(user_id):
 def change_password(user_id):
     new_password = request.form.get('password')
     new_repeat_password = request.form.get('repeat_password')
-    print(new_password, new_repeat_password)
     if new_password is not None:
         if new_repeat_password is not None:
             if new_password == new_repeat_password:
                 if len(new_password) >= 8:
-                    print(new_password, new_repeat_password)
                     user = USERS.query.filter_by(id=user_id).first()
                     user.password = generate_password_hash(new_password)
                     user.repeat_password = generate_password_hash(new_repeat_password)
@@ -321,7 +325,8 @@ def admin_create_post():
     if len(text) >= 1:
         if len(author) > 2:
             if len(title) >= 1:
-                db.session.add(POSTS(title=title, author=author, text=text))
+                user = USERS.query.filter_by(username=author).first()
+                db.session.add(POSTS(title=title, author=author, text=text, email_author=user.email))
                 db.session.commit()
     return redirect(url_for("posts_control"))
 
@@ -411,6 +416,175 @@ def change_text(post_id):
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+# LSTM model
+# ////////////////////////////////////////////////////////
+
+def create_inout_sequences(input_data, tw):
+    inout_seq = []
+    L = len(input_data)
+    for i in range(L - tw):
+        train_seq = input_data[i:i + tw]
+        train_label = input_data[i + tw:i + tw + 1]
+        inout_seq.append((train_seq, train_label))
+    return inout_seq
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+        self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size),
+                            torch.zeros(1, 1, self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq), 1, -1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
+
+
+@app.route('/LSTM/activate/', methods=['GET', 'POST'])
+def LSTM_n():
+    flight_data = sns.load_dataset("flights")
+    if LSTM_m.query.count() != 0:
+        LSTM_m.query.delete()
+        db.session.commit()
+
+    data_year = flight_data['year'].values.astype(int)[-12:]
+    data_month = flight_data['month'].values.astype(str)[-12:]
+    data_passengers = flight_data['passengers'].values.astype(float)
+
+    test_data_size = 12
+    train_data_pass = data_passengers[:-test_data_size]
+    test_data_pass = data_passengers[-test_data_size:]
+
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    train_data_normalized = scaler.fit_transform(train_data_pass.reshape(-1, 1))
+    train_data_normalized = torch.FloatTensor(train_data_normalized).view(-1)
+
+    train_window = 12
+    train_inout_seq = create_inout_sequences(train_data_normalized, train_window)
+
+    model = LSTM()
+    loss_function = nn.MSELoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    epochs = 150
+
+    for i in range(epochs):
+        for seq, labels in train_inout_seq:
+            optimizer.zero_grad()
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
+                                 torch.zeros(1, 1, model.hidden_layer_size))
+            y_pred = model(seq)
+            single_loss = loss_function(y_pred, labels)
+            single_loss.backward()
+            optimizer.step()
+        if i % 25 == 1:
+            print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
+    print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+
+    fut_pred = 12
+    test_inputs = train_data_normalized[-train_window:].tolist()
+    model.eval()
+
+    for i in range(fut_pred):
+        seq = torch.FloatTensor(test_inputs[-train_window:])
+        with torch.no_grad():
+            model.hidden = (torch.zeros(1, 1, model.hidden_layer_size),
+                            torch.zeros(1, 1, model.hidden_layer_size))
+            test_inputs.append(model(seq).item())
+
+    predictions = scaler.inverse_transform(np.array(test_inputs[fut_pred:]).reshape(-1, 1))
+
+    for i in range(fut_pred):
+        db.session.add(LSTM_m(year=data_year[i], month=data_month[i], passengers=test_data_pass[i],
+                              predictions=predictions[i][0],
+                              wrong_answer=test_data_pass[i] - predictions[i][0]))
+
+    db.session.commit()
+
+    return redirect(url_for("LSTM_site_view"))
+
+
+@app.route('/LSTM/', methods=['GET', 'POST'])
+def LSTM_site_view():
+    form = RegistrationForm()
+    form1 = LoginForm()
+    lstm = LSTM_m.query.all()
+    c_lstm = LSTM_m.query.count()
+    return render_template("LSTM.html", form=form, form1=form1, admin_key=admin_key, lstm=lstm, c_lstm=c_lstm)
+
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+# Search
+# ////////////////////////////////////////////////////////
+@app.route('/search')
+def search():
+    search = '%{}%'.format(request.args.get('search'))
+    form = RegistrationForm()
+    form1 = LoginForm()
+    page = request.args.get('page', 1, type=int)
+    posts = POSTS.query.filter(POSTS.title.like(search) | POSTS.author.like(search) | POSTS.text.like(search)).paginate(
+        page=page, per_page=3)
+    return render_template("search.html", form=form, form1=form1, admin_key=admin_key, posts=posts, search=search)
+
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+# Big search
+# ////////////////////////////////////////////////////////
+@app.route('/big_search', methods=['GET', 'POST'])
+def big_search():
+    search = '%{}%'.format(request.args.get('search'))
+    chbox = request.args.getlist('chbox')
+    chbox.sort()
+    form = RegistrationForm()
+    form1 = LoginForm()
+    posts = ""
+    if chbox == ['1']:
+        posts = POSTS.query.filter(POSTS.author.like(search))
+    if chbox == ['2']:
+        posts = POSTS.query.filter(POSTS.email_author.like(search))
+    if chbox == ['3']:
+        posts = POSTS.query.filter(POSTS.title.like(search))
+    if chbox == ['4']:
+        posts = POSTS.query.filter(POSTS.text.like(search))
+    if chbox == ['1', '2']:
+        posts = POSTS.query.filter(POSTS.author.like(search) & POSTS.email_author.like(search))
+    if chbox == ['2', '3']:
+        posts = POSTS.query.filter(POSTS.email_author.like(search) & POSTS.title.like(search))
+    if chbox == ['2', '4']:
+        posts = POSTS.query.filter(POSTS.email_author.like(search) & POSTS.text.like(search))
+    if chbox == ['3', '4']:
+        posts = POSTS.query.filter(POSTS.title.like(search) & POSTS.text.like(search))
+    if chbox == ['1', '3']:
+        posts = POSTS.query.filter(POSTS.author.like(search) & POSTS.title.like(search))
+    if chbox == ['1', '4']:
+        posts = POSTS.query.filter(POSTS.author.like(search) & POSTS.text.like(search))
+    if chbox == ['1', '2', '3']:
+        posts = POSTS.query.filter(
+            POSTS.author.like(search) & POSTS.email_author.like(search) & POSTS.title.like(search))
+    if chbox == ['1', '2', '4']:
+        posts = POSTS.query.filter(
+            POSTS.author.like(search) & POSTS.email_author.like(search) & POSTS.text.like(search))
+    if chbox == ['1', '3', '4']:
+        posts = POSTS.query.filter(POSTS.author.like(search) & POSTS.title.like(search) & POSTS.text.like(search))
+    if chbox == ['2', '3', '4']:
+        posts = POSTS.query.filter(POSTS.email_author.like(search) & POSTS.title.like(search) & POSTS.text.like(search))
+    if chbox == ['1', '2', '3', '4']:
+        posts = POSTS.query.filter(
+            POSTS.author.like(search) & POSTS.email_author.like(search) & POSTS.title.like(search) & POSTS.text.like(
+                search))
+    return render_template("big_search.html", form=form, form1=form1, admin_key=admin_key, posts=posts)
+
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
